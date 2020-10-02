3a65c59b823cb262dfcf69882b5a080dad246982
Sam King <samuelking94@gmail.com>
2020-10-02 19:30:24 +0100

    simple game initial commit

---
+++ food.js
@@ -0,0 +1,20 @@
+import { onSnake, expandSnake } from './snake.js'
+import { randomGridPosition } from './grid.js'
+
+let food = { x: 10, y: 1 };
+const EXPANSION_RATE = 1;
+
+export function update() {
+    if (onSnake(food)) {
+        expandSnake(EXPANSION_RATE);
+        food = randomGridPosition();
+    }
+}
+
+export function draw(gameBoard) {
+    const foodElement = document.createElement('div');
+    foodElement.style.gridRowStart = food.y;
+    foodElement.style.gridColumnStart = food.x;
+    foodElement.classList.add('food');
+    gameBoard.appendChild(foodElement);
+}
